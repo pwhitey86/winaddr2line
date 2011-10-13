9f1fe868d33f129b1c56e1cbf4039b10c09a1799
raymond <raymond@localhost>
2011-10-13 12:42:19 +0800

    fix bug in winnm
    - remove redundant log
    - correct help message

--- winnm/winnm.cpp
+++ winnm/winnm.cpp
@@ -9,7 +9,7 @@
 #include <windows.h>
 #include "dbghelp.h"
 
-#define    VERSION                TEXT("1.0.0")
+#define    VERSION                TEXT("1.0.1")
 
 #define OPT_PRINT_FILE_NAME   TEXT("-A")
 #define OPT_PRINT_FILE_NAME_L TEXT("--print-file-name")
@@ -73,39 +73,14 @@ void print_help()
 {
 	printf("%s\r\n", 
 		"Usage: winnm [option(s)] [file(s)]\n\
-		List symbols in [file(s)] (a.out by default).\n\
+		List symbols in [file(s)] (a.exe by default).\n\
 		The options are:\n\
-		-a, --debug-syms       Display debugger-only symbols\n\
 		-A, --print-file-name  Print name of the input file before every symbol\n\
-		-B                     Same as --format=bsd\n\
-		-C, --demangle[=STYLE] Decode low-level symbol names into user-level names\n\
-		The STYLE, if specified, can be `auto' (the default),\n\
-		`gnu', `lucid', `arm', `hp', `edg', `gnu-v3', `java'\n\
-		or `gnat'\n\
-		--no-demangle      Do not demangle low-level symbol names\n\
-		-D, --dynamic          Display dynamic symbols instead of normal symbols\n\
-		--defined-only     Display only defined symbols\n\
-		-e                     (ignored)\n\
-		-f, --format=FORMAT    Use the output format FORMAT.  FORMAT can be `bsd',\n\
-		`sysv' or `posix'.  The default is `bsd'\n\
-		-g, --extern-only      Display only external symbols\n\
+		-C, --demangle         Decode low-level symbol names into user-level names\n\
 		-l, --line-numbers     Use debugging information to find a filename and\n\
-		line number for each symbol\n\
+							line number for each symbol\n\
 		-n, --numeric-sort     Sort symbols numerically by address\n\
 		-o                     Same as -A\n\
-		-p, --no-sort          Do not sort the symbols\n\
-		-P, --portability      Same as --format=posix\n\
-		-r, --reverse-sort     Reverse the sense of the sort\n\
-		-S, --print-size       Print size of defined symbols\n\
-		-s, --print-armap      Include index for symbols from archive members\n\
-		--size-sort        Sort symbols by size\n\
-		--special-syms     Include special symbols in the output\n\
-		--synthetic        Display synthetic symbols as well\n\
-		-t, --radix=RADIX      Use RADIX for printing symbol values\n\
-		--target=BFDNAME   Specify the target object format as BFDNAME\n\
-		-u, --undefined-only   Display only undefined symbols\n\
-		-X 32_64               (ignored)\n\
-		@FILE                  Read options from FILE\n\
 		-h, --help             Display this information\n\
 		-V, --version          Display this program's version number\n");
 }
@@ -211,7 +186,7 @@ TCHAR* get_next_file(option &opt, int index)
 		return NULL;
 	file_node* node = opt.file_list;
 	int i = 0;
-		_tprintf(TEXT("%d %d\n"), i, index);
+		//_tprintf(TEXT("%d %d\n"), i, index);
 	while(i < index)
 	{
 		node = node->next;
@@ -327,9 +302,11 @@ int nm(option &opt)
 	TCHAR* module = get_next_file(opt, i);
 	while(NULL != module)
 	{
-		_tprintf(TEXT("%d error %d  %s\n"), __LINE__, i, module);
 		if(!get_file_attr(module, base_addr, file_size))
+		{
+			_tprintf(TEXT("get_file_attr %d error %d  %s\n"), __LINE__, i, module);
 			return 1;
+		}
 		load_addr = SymLoadModuleEx(proc, NULL, module, NULL, base_addr,
 			file_size, NULL, 0);
 		if(0 == load_addr)
@@ -338,7 +315,7 @@ int nm(option &opt)
 		}
 		user_context uc(&opt, module);
 		if(!SymEnumSymbols(proc, base_addr, "*!*", on_enum_symbol, &uc))       
-			_tprintf(TEXT("%d error %u\n"), __LINE__, GetLastError());
+			_tprintf(TEXT("SymEnumSymbols failed. errorno: %u\n"), __LINE__, GetLastError());
 
 		SymUnloadModule64(proc, load_addr);
 		module = get_next_file(opt, ++i);
